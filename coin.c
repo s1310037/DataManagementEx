65e633258a45670323ab1497ae34826e3c27a30d
s1310037 <s1310037@u-aizu.ac.jp>
2024-07-04 21:54:38 +0900

    HoT: initial revision

---
+++ coin.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+int main(){
+    int coin, count = 0;
+
+    printf("Tossing a coin...\n");
+        for(int i = 0; i < 3; i++){
+            coin = rand();
+            if(coin % 2 = 0){
+            printf("Round %d: Heads\n", i);
+            count++;
+            }
+            else printf("Round %d: Tails\n", i);
+    }
+    printf("Heads: %d, Tail: %d\n"count, 3-count);
+}
